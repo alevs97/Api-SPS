fd102c43a1568a9a681a372755c5bdf75a1d1334
alevs97 <alejandro.viveros.s@outlook.es>
2020-10-01 11:26:24 -0500

    Correccion de puertos

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
     //Instanciamos el puerto como una constante
     //Escogemos el puerto 8090
-    port: process.env.PORT || 8090,
+    port: process.env.PORT || 8091,
     //conexion a la base de datos. Defecto: 27017. Enlazamos al servicio de mongo DOcker
     db: process.env.MONGODB || 'mongodb://mongo/registro'
 }
